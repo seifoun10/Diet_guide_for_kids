e0c9b962f0a0aebfd54f2133bb71edc85a6fe40b
tictacf12 <tictacf12@192.168.1.64>
2014-03-02 15:49:49 +0100

    dffedf

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -2,14 +2,12 @@
 
 package dao;
 
-import entities.Parent;
 import entities.User;
 import util.MyConnection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 /**
