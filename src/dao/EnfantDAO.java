13265fa558274b8e37494b769ad49036fac859f8
tictacf12 <tictacf12@192.168.1.64>
2014-03-02 15:26:00 +0100

    dffedf

--- src/dao/EnfantDAO.java
+++ src/dao/EnfantDAO.java
@@ -19,5 +19,34 @@ import java.util.List;
  * @author tictacf12
  */
 public class EnfantDAO {
+    public List<Enfant> getEnfantList(String parent){
+        
+        List<Enfant> listEnfant = new ArrayList<Enfant>();
+        
+        String requete = "SELECT * FROM enfants WHERE Login=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setString(1, parent+"%");
+            
+            ResultSet resultat = ps.executeQuery();
+            while(resultat.next()){
+                Enfant enfant =new Enfant();
+                enfant.setId(resultat.getInt("Id"));
+                enfant.setNom(resultat.getString("Nom"));
+                enfant.setPrenom(resultat.getString("Prenom"));
+                enfant.setDateNaissance(resultat.getDate("Date_De_Naissance"));
+                enfant.setSexe(resultat.getBoolean("Sexe"));
+                enfant.setNationalite(resultat.getString("Nationalite"));
+                enfant.setDateAjout(resultat.getDate("Date_Ajout"));
+                enfant.setImcActuel(resultat.getFloat("IMC_Actuelle"));
+                enfant.setImcAjout(resultat.getFloat("IMC_Ajout"));
+                listEnfant.add(enfant);
+            }
+            return listEnfant;
+        }catch (SQLException ex){
+            System.out.println("Erreur lors de la recherche dans la table enfants");
+            return null;
+        }
+    }
     
 }
